a07fe6946491a6b1f59df14d0dcdb5a31c0f9fbc
Alejandra Paez <alepaezdev@gmail.com>
2023-06-09 22:04:50 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+require('dotenv').config() // lo mas pronto de nuestra aplicacion;
+const express = require("express");
+const mongoose = require("mongoose");
+
+/**
+ * 1. Conexion a base de datos
+ * 2. Prender servidor
+ */
+
+const { DB_USERNAME, DB_PASSWORD, DB_HOST, DB_NAME } = process.env
+// Vamos a ocultar
+// Variables de entorno
+const databaseURL = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`;
+
+mongoose.connect(databaseURL)
+.then(() => {
+  console.log("We are connected to our database :)")
+})
+.catch((err) => {
+  console.log("We have an error", err);
+})
